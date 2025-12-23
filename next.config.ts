de26b391c7da4e56d7214bda03869a8d190056e7
최정환 <choejeonghwan@choejeonghwan-ui-MacBookPro.local>
2025-12-24 00:57:27 +0900

    chore(config): add unsplash to image domains

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "tocgolezortnknflfian.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
   experimental: {
